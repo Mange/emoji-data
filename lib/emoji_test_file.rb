98cdc56b070134dfd913e5851c717bc0d2bdf8cb
Magnus Bergmark <me@mange.dev>
2021-05-10 22:32:16 +0200

    Refactor EmojiTestFile

--- lib/emoji_test_file.rb
+++ lib/emoji_test_file.rb
@@ -8,25 +8,55 @@ class EmojiTestFile
   end
 
   def each_emoji
-    File.open(@filename, "r") do |file|
-      current_group = nil
-      current_subgroup = nil
+    return to_enum(:each_emoji) unless block_given?
 
-      file.each_line do |line|
-        if line.start_with?("# group: ")
-          current_group = line.split(": ", 2).last.strip
-        elsif line.start_with?("# subgroup: ")
-          current_subgroup = line.split(": ", 2).last.strip
-        elsif (matches = MATCHER.match(line))
-          yield Emoji.new(
-            characters: matches["characters"],
-            category: current_group,
-            subcategory: current_subgroup,
-            name: matches["name"],
-            qualification: matches["qualification"]
-          )
-        end
+    each_line_by_category do |line, current_group, current_subgroup|
+      emoji = parse_emoji_line(line, current_group, current_subgroup)
+      yield emoji if emoji
+    end
+  end
+
+  private
+
+  def each_line_by_category
+    group = nil
+    subgroup = nil
+
+    each_line do |line|
+      if (value = comment_value("group", line))
+        group = value
+      elsif (value = comment_value("subgroup", line))
+        subgroup = value
+      else
+        yield line, group, subgroup
       end
+    end
+  end
+
+  def each_line
+    File.open(@filename, "r") do |file|
+      file.each_line do |line|
+        yield line
+      end
+    end
+  end
+
+  def comment_value(type, line)
+    prefix = "# #{type}: "
+    if line.start_with?(prefix)
+      line.sub(prefix, "").strip
+    end
+  end
+
+  def parse_emoji_line(line, current_group, current_subgroup)
+    if (matches = MATCHER.match(line))
+      Emoji.new(
+        category: current_group,
+        subcategory: current_subgroup,
+        characters: matches["characters"],
+        name: matches["name"],
+        qualification: matches["qualification"]
+      )
     end
   end
 
